962deb04405ac2d40f5db327ebbc51d31ce90604
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-11-12 18:02:05 -0800

    Fixed nav and added interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+interface Candidate {
+    id: number;
+    name: string;
+    username: string;
+    location: string;
+    avatar_url: string;
+    html_url: string;
+    email: string;
+    company: string;
+}
